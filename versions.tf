5eac18c7b5d4f018f1a08ff43f7ced84f5301574
Abdullah VSC <abdullah@azasoft.co.uk>
2025-05-24 19:30:29 +0100

    feat: Add initial S3 bucket terraform and Github Actions workflow

---
+++ versions.tf
@@ -0,0 +1,17 @@
+# versions.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "abdullah2202-cicd-terraform-bucket" # Replace with a globally unique name for your state bucket
+    key            = "terraform.tfstate"
+    region         = "us-east-1" # Must be the same region as your resources or where you want to store state
+    encrypt        = true
+    dynamodb_table = "abdullah2202-cicd-terraform-state-lock-table" # Replace with a unique name for your DynamoDB lock table
+  }
+}
